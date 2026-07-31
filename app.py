8f5ecf13c6870f5d49e4961686629e141daac3ef
jasneet5683 <jasneet.happy@gmail.com>
2026-07-31 17:23:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -131,3 +131,4 @@ if __name__ == '__main__':
     # Railway typically uses the PORT environment variable
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
